fff90f7a9097b5f6b3943c7c70d5a4fbabe73bda
Zecharias Mengis <zmengis@gmail.com>
2021-04-12 10:51:06 -0500

    commit tree 470dd9355bd91a2e78c85b04e7df32e24da1564c

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,5 +11,18 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  # add create method here
+  # def create
+  #   post = Post.new
+  #   post.title = params[:title]
+  #   post.description = params[:description]
+  #   post.save
+  # end
+
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
+  end 
 end
